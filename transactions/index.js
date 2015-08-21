62d088f3b0da30a50fd0affa00b2949baff81b4a
Ken Stowell <kstowell@rubicon.com>
2015-08-21 08:41:07 -0700

    Fixed some reference problems by moving the return object to the lower scope.

--- transactions/index.js
+++ transactions/index.js
@@ -6,12 +6,50 @@
  * @description Creates Transaction objects. Not super robust at the moment.
  */
 module.exports = function(spec) {
-  console.log(spec);
+  // Create state defaults
+  spec = spec || {};
+  spec.struct = spec.struct || 'Not Specified'
+
   return {
-    struct: spec.struct,
-    type: spec.type,
-    time: Date.now(),
-    data: spec.data,
-    options: spec.options
+    Write: Transaction('write'),
+    Read: Transaction('read'),
+    Delete: Transaction('delete')
   };
+
+  /**
+   * Creates an execution context with the transaction type.
+   * @param  {[type]} desc [description]
+   * @return {[type]}      [description]
+   */
+  function Transaction(desc) {
+
+    return function(data) {
+      var transaction = {};
+          transaction.type = desc;
+
+      data = data || {};
+      
+      // Copy any data options into the transaction
+      for(var obj in data) {
+        transaction[obj] = data[obj];
+      }
+
+      // Copy spec reference into transaction.
+      // Abstract this into a lib.
+      for(var key in spec) {
+        transaction[key] = spec[key];
+      }
+
+      return {
+        success: function(msg) {
+          transaction.success = msg || 'Transaction Successful';
+          return transaction;
+        },
+        error: function(msg) {
+          transaction.error = msg || 'An error occured';
+          return transaction;
+        }
+      };
+    };
+  }
 };
